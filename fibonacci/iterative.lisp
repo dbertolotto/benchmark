3a269986bddaceb17ddf2c166abbd8e6e840d12e
Davide Bertolotto <davide.bertolotto@netcetera.com>
2019-03-26 18:15:20 +0100

    added common lisp routines

---
+++ fibonacci/iterative.lisp
@@ -0,0 +1,17 @@
+(defun fib-iter (n)
+  (let ((a 0) (b 1) (c n))
+    (loop for i from 2 to n do
+          (setq c (+ a b)
+                a b
+                b c))
+    c))
+
+(defun limit (s l)
+  (if (<= (length s) l)
+      s
+      (concatenate 'string (subseq s 0 (- l 1)) "+")))
+
+(let* ((n 50000)
+       (fib (time (fib-iter n)))
+       (fibl (limit (format nil "~A" fib) 10)))
+  (format t "n=~A fib=~A~%" n fibl))
---
+++ fibonacci/recursive.lisp
@@ -0,0 +1,14 @@
+(defun fib-rec (n)
+  (if (< n 2)
+    n
+    (+ (fib-rec (- n 2)) (fib-rec (- n 1)))))
+
+(defun limit (s l)
+  (if (<= (length s) l)
+      s
+      (concatenate 'string (subseq s 0 (- l 1)) "+")))
+
+(let* ((n 35)
+       (fib (time (fib-rec n)))
+       (fibl (limit (format nil "~A" fib) 10)))
+  (format t "n=~A fib=~A~%" n fibl))
