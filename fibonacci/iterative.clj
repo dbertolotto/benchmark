c47707a25c3cb5cf53ccae17897bf11adee43d95
Davide Bertolotto <davide.bertolotto@gmail.com>
2019-07-23 21:27:45 +0200

    added clojure examples

---
+++ fibonacci/iterative.clj
@@ -0,0 +1,18 @@
+(defn fib-iter [n]  
+    (let [fib (map first (iterate (fn [[a b]] [b (+' a b)]) [0 1]))]
+      (nth fib n)))
+
+(defn limit [o l]
+  (let [s (str o)
+        c (count s)]
+    (if (> c l)
+      (subs s 0 l)
+      s)))
+
+(defn main []
+  (let [n 50000
+      fib (time (fib-iter n))]
+    (println 
+      (format "n=%d, fib=%s" n (limit fib 10)))))
+
+(main)
---
+++ fibonacci/recursive.clj
@@ -0,0 +1,21 @@
+(defn fib-rec [n]
+  (case n
+    0 0
+    1 1
+    (+ (fib-rec (- n 1))
+       (fib-rec (- n 2)))))
+
+(defn limit [o l]                                 
+  (let [s (str o)
+        c (count s)]
+    (if (> c l)               
+      (subs s 0 l)                              
+      s)))
+
+(defn main []
+  (let [n 35                           
+        fib (time (fib-rec n))]            
+    (println
+      (format "n=%d, fib=%s" n (limit fib 10)))))
+
+(main)
