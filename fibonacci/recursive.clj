f698bf8ae40a4f1cc6b20fbbb22a4e23c820f60c
Davide Bertolotto <davide.bertolotto@gmail.com>
2020-02-04 18:11:55 +0000

    add variations to recursive java and clj

--- fibonacci/recursive.clj
+++ fibonacci/recursive.clj
@@ -7,10 +7,22 @@
     (+ (fib-rec (- n 1))
        (fib-rec (- n 2)))))
 
-(defn main []
-  (let [n 35                           
-        fib (time (fib-rec n))]            
+(defn ^Long fib-rec-long [n]
+  (case n
+    0 0
+    1 1
+    (+ (fib-rec-long (- n 1))
+       (fib-rec-long (- n 2)))))
+
+(defn fibo [n f s]
+  (println s)
+  (let [fib (time (f n))]     
     (println
       (format "n=%d, fib=%s" n (limit fib 10)))))
+
+(defn main []     
+  (let [n 35]
+    (fibo n fib-rec "* plain")
+    (fibo n fib-rec-long "*Long")))
 
 (main)
